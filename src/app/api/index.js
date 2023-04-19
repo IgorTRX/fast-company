d9f4ba29783594c82ef6d7b150faf994d9b5f4fc
IgorTRX <nhdigor@gmail.com>
2023-04-19 17:22:36 +0300

    Lesson9 Forms. Part 2

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,4 +1,4 @@
-import users from './fake.api/user.api'
+import users from './fake.api/users.api'
 import professions from './fake.api/professions.api'
 import qualities from './fake.api/qualities.api'
 
--- src/app/components/common/form/selectField.jsx
+++ src/app/components/common/form/selectField.jsx
@@ -22,7 +22,7 @@ const SelectField = ({
     !Array.isArray(options) && typeof options === 'object'
       ? Object.values(options)
       : options
-  console.log(optionsArray.length)
+
   return (
     <div className="mb-4">
       <label htmlFor={name} className="form-label">
