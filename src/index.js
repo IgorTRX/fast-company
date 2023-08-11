3b84dddd97b757c4966fc288af36dcb137e82320
IgorTRX <nhdigor@gmail.com>
2023-08-12 01:02:09 +0300

    20. Внедрение Redux в проект. Fast Company / 2. Создаем Store

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
+import { createStore } from './app/store/createStore'
+import { Provider } from 'react-redux'
+
+const store = createStore()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter>
-    {/* <React.StrictMode> */}
-    <App />
-    {/* </React.StrictMode> */}
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      {/* <React.StrictMode> */}
+      <App />
+      {/* </React.StrictMode> */}
+    </BrowserRouter>
+  </Provider>
 )
 
 // If you want to start measuring performance in your app, pass a function
