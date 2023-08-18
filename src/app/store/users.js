859e820b533064df42c13c2b73834929b7474cb7
IgorTRX <nhdigor@gmail.com>
2023-08-18 15:43:46 +0300

    20. Внедрение Redux в проект. Fast Company / 10. Переносим метод создания пользователя в User Slice

--- src/app/store/users.js
+++ src/app/store/users.js
@@ -2,6 +2,7 @@ import { createAction, createSlice } from '@reduxjs/toolkit'
 import userService from '../service/user.service'
 import authService from '../service/auth.service'
 import localStorageService from '../service/localStorage.service'
+import getRandomInt from '../utils/getRandomInt'
 
 const usersSlice = createSlice({
   name: 'users',
@@ -29,6 +30,12 @@ const usersSlice = createSlice({
     },
     authRequestFailed(state, action) {
       state.error = action.payload
+    },
+    userCreated(state, action) {
+      if (!Array.isArray(state.entities)) {
+        state.entities = []
+      }
+      state.entities.push(action.payload)
     }
   }
 })
@@ -39,10 +46,13 @@ const {
   usersReceived,
   usersRequestFailed,
   authRequestSuccess,
-  authRequestFailed
+  authRequestFailed,
+  userCreated
 } = actions
 
 const authRequested = createAction('users/authRequested')
+const userCreateRequested = createAction('users/userCreateRequested')
+const createUserFailed = createAction('users/createUserFailed')
 
 export const signUp =
   ({ email, password, ...rest }) =>
@@ -52,10 +62,36 @@ export const signUp =
       const data = await authService.register({ email, password })
       localStorageService.setTokens(data)
       dispatch(authRequestSuccess({ userId: data.localId }))
+      dispatch(
+        createUser({
+          _id: data.localId,
+          email,
+          rate: getRandomInt(1, 5),
+          completedMeetings: getRandomInt(0, 200),
+          image: `https://avatars.dicebear.com/api/avataaars/${(
+            Math.random() + 1
+          )
+            .toString(36)
+            .substring(7)}.svg`,
+          ...rest
+        })
+      )
     } catch (error) {
       dispatch(authRequestFailed(error.message))
     }
   }
+
+function createUser(payload) {
+  return async function (dispatch) {
+    dispatch(userCreateRequested())
+    try {
+      const { content } = await userService.create(payload)
+      dispatch(userCreated(content))
+    } catch (error) {
+      dispatch(createUserFailed(error.message))
+    }
+  }
+}
 
 export const loadUsersList = () => async (dispatch) => {
   dispatch(usersRequested())
