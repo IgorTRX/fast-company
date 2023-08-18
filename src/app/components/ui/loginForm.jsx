17498d0680b30c72605ba7492c7b5423845ba275
IgorTRX <nhdigor@gmail.com>
2023-08-19 01:13:06 +0300

    20. Внедрение Redux в проект. Fast Company / 12. Переносим метод входа в User Slice

--- src/app/components/ui/loginForm.jsx
+++ src/app/components/ui/loginForm.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { validator } from '../../utils/validator'
 import TextField from '../common/form/textField'
 import CheckBoxField from '../common/form/checkBoxField'
-import { useAuth } from '../../hooks/useAuth'
 import { useHistory } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { login } from '../../store/users'
 
 const LoginForm = () => {
   const history = useHistory()
-  const { signIn } = useAuth()
+  const dispatch = useDispatch()
   const [data, setData] = useState({ email: '', password: '', stayOn: false })
   const [errors, setErrors] = useState({})
   const [enterError, setEnterError] = useState(null)
@@ -38,20 +39,16 @@ const LoginForm = () => {
 
   const isValid = Object.keys(errors).length === 0
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
     const isValid = validate()
     if (!isValid) return
-    try {
-      await signIn(data)
-      history.push(
-        history.location.state.from.pathname
-          ? history.location.state.from.pathname
-          : '/'
-      )
-    } catch (error) {
-      setEnterError(error.message)
-    }
+
+    const redirect = history.location.state
+      ? history.location.state.from.pathname
+      : '/'
+
+    dispatch(login({ payload: data, redirect }))
   }
 
   return (
--- src/app/store/users.js
+++ src/app/store/users.js
@@ -55,6 +55,21 @@ const authRequested = createAction('users/authRequested')
 const userCreateRequested = createAction('users/userCreateRequested')
 const createUserFailed = createAction('users/createUserFailed')
 
+export const login =
+  ({ payload, redirect }) =>
+  async (dispatch) => {
+    const { email, password } = payload
+    dispatch(authRequested())
+    try {
+      const data = await authService.login({ email, password })
+      dispatch(authRequestSuccess({ userId: data.localId }))
+      localStorageService.setTokens(data)
+      history.push(redirect)
+    } catch (error) {
+      dispatch(authRequestFailed(error.message))
+    }
+  }
+
 export const signUp =
   ({ email, password, ...rest }) =>
   async (dispatch) => {
