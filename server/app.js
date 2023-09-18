9088087887be88e7e81ecdfa599dcb1b91811f84
IgorTRX <nhdigor@gmail.com>
2023-09-18 18:25:41 +0300

    Backend / 5. Создаем Backend. Часть 1 / 4. Настройка рабочего окружения

--- server/app.js
+++ server/app.js
@@ -7,6 +7,12 @@ const app = express()
 
 const PORT = config.get('port') ?? 8080
 
+// if (process.env.NODE_ENV === 'production') {
+//   console.log('Productions')
+// } else {
+//   console.log('Development')
+// }
+
 app.listen(PORT, () => {
   console.log(chalk.green(`Server has been started on port ${PORT}`))
 })
