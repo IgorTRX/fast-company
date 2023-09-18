107563a06e9a0311179f135f99502bc2eeb64bd8
IgorTRX <nhdigor@gmail.com>
2023-09-18 19:06:36 +0300

    Backend / 5. Создаем Backend. Часть 1 / 5. Добавляем middleware для приложения

--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ const chalk = require('chalk')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 const PORT = config.get('port') ?? 8080
 
 // if (process.env.NODE_ENV === 'production') {
