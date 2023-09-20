2799cdad42dce3e38f6b6b00b34ba98e5af91071
IgorTRX <nhdigor@gmail.com>
2023-09-21 01:03:01 +0300

    Backend / 5. Создаем Backend. Часть 1 / 11. Получаем Профессии и Качества

--- server/routes/quality.routes.js
+++ server/routes/quality.routes.js
@@ -1,4 +1,16 @@
 const express = require('express')
+const Quality = require('../models/Quality')
 const router = express.Router({ mergeParams: true })
+
+router.get('/', async (req, res) => {
+  try {
+    const qualities = await Quality.find()
+    res.status(200).send(qualities)
+  } catch (error) {
+    res
+      .status(500)
+      .json({ massage: 'На сервере произошла ошибка. Попробуйте позже.' })
+  }
+})
 
 module.exports = router
